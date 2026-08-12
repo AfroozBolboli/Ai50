f965ee4f1362fff8e70b7371de55c598309157bf
Afrooz Bolboli <68796416+shigurehyung@users.noreply.github.com>
2026-08-12 15:16:56 +0200

    Add the player function.

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -19,16 +19,33 @@ def initial_state():
 
 
 def player(board):
+
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
 
+    # Count the empty cells in the board
+    empty_count = 0
+    for row in board:
+        for cell in row:
+            if cell == EMPTY:
+                empty_count += 1
+
+    # If empty cells are odd then it is X's turn, otherwise it is O's turn
+    if empty_count == 0:
+        raise Exception("No empty slots.")
+    elif empty_count % 2 == 0:
+        return O
+    elif empty_count % 2 == 1: 
+        return X
+    
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
+    i is row j is column range 0, 1, and 2
     """
+    # Basically return the coordinates of empties
     raise NotImplementedError
 
 
@@ -42,7 +59,10 @@ def result(board, action):
 def winner(board):
     """
     Returns the winner of the game, if there is one.
+    One can win the game with three of their moves in 
+    a row horizontally, vertically, or diagonally.
     """
+    #there will be at most one winner 
     raise NotImplementedError
 
 
@@ -50,18 +70,31 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+
+    print("I am in terminal function")
+    if not EMPTY in board:
+        return True
+    else:
+        return False
+
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
+
+    # assume utility will only be called on a board if terminal(board) is True
     raise NotImplementedError
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
+    """
+
+    """
+    The move returned should be the optimal action (i, j) that is one of the allowable actions on the board. If multiple moves are equally optimal, any of those moves is acceptable.
+    If the board is a terminal board, the minimax function should return None.
     """
     raise NotImplementedError
