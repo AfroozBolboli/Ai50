69ae2e89dd5657cbcd211859cd7cd3d407b985c6
Afrooz Bolboli <68796416+shigurehyung@users.noreply.github.com>
2026-08-24 16:14:27 +0200

    style: add space for readability

--- knights/puzzle.py
+++ knights/puzzle.py
@@ -9,62 +9,77 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
-# Each speaker makes exactly one statement
-
 # Puzzle 0
 # A says "I am both a knight and a knave."
+
 knowledge0 = And(
-    Or(AKnight,AKnave),
-    Not(And(AKnight,AKnave)),
-    Biconditional(AKnight,And(AKnight,AKnave))
+    # A can either be Knight or Knave and not both.
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    # A says "I am both a knight and a knave."
+    Biconditional(AKnight, And(AKnight, AKnave))
 )
+
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 
 knowledge1 = And(
-    # The base rule of the world
-    Or(AKnight,AKnave),
-    Or(BKnight,BKnave),
-    Not(And(AKnight,AKnave)),
-    Not(And(BKnight,BKnave)),
+    # They can either be Knight or Knave and not both.
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
 
-    Biconditional(AKnight, And(AKnave,BKnave)),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    # A says "We are both knaves." 
+    Biconditional(AKnight, And(AKnave, BKnave)),
 )
+
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
-knowledge2 = And(
-    # The base rule of the world
-    Or(AKnight,AKnave),
-    Or(BKnight,BKnave),
-    Not(And(AKnight,AKnave)),
-    Not(And(BKnight,BKnave)),
 
-    Biconditional(AKnight, Or(And(AKnight,BKnight),And(AKnave,BKnave))),
-    Biconditional(BKnight,Or(And(AKnight,BKnave),And(AKnave,BKnight)))
+knowledge2 = And(
+    # They can either be Knight or Knave and not both.
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    # A says "We are the same kind."
+    Biconditional(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    # B says "We are of different kinds."
+    Biconditional(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight)))
 )
+
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+
 knowledge3 = And(
-    # The base rule of the world
-    Or(AKnight,AKnave),
-    Or(BKnight,BKnave),
-    Or(CKnight,CKnave),
+    # They can either be Knight or Knave and not both.
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
 
-    Not(And(AKnight,AKnave)),
-    Not(And(BKnight,BKnave)),
-    Not(And(CKnight,CKnave)),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
 
-    Biconditional(BKnight,CKnave),
-    Biconditional(BKnight,Biconditional(AKnight,AKnave)),
-    Biconditional(CKnight,AKnight)
+    # B says "A said 'I am a knave'."
+    Biconditional(BKnight, Biconditional(AKnight, AKnave)),
+    # B says "C is a knave."
+    Biconditional(BKnight, CKnave),
+    # C says "A is a knight."
+    Biconditional(CKnight, AKnight)
 )
 
 
